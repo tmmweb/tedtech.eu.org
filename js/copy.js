5d193cea4e92353cad9bd697cce5e22550f21dc0
virtualtedtech <76200915+virtualtedtech@users.noreply.github.com>
2020-12-18 13:22:29 +0530

    Add files via upload

---
+++ js/copy.js
@@ -0,0 +1,110 @@
+function copyToClipboard() {
+    const copyText = document.getElementById("p1").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard2() {
+    const copyText = document.getElementById("p2").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn2");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard3() {
+    const copyText = document.getElementById("p3").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn3");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard4() {
+    const copyText = document.getElementById("p4").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn4");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard5() {
+    const copyText = document.getElementById("p5").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn5");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard6() {
+    const copyText = document.getElementById("p6").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn6");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard7() {
+    const copyText = document.getElementById("p7").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn7");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard8() {
+    const copyText = document.getElementById("p8").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn8");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard9() {
+    const copyText = document.getElementById("p9").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn9");
+    tooltip.innerHTML = "Copied!";
+}
+function copyToClipboard10() {
+    const copyText = document.getElementById("p10").textContent;
+    const textArea = document.createElement('textarea');
+    textArea.textContent = copyText;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy");
+    document.body.removeChild(textArea);
+    var tooltip = document.getElementById("mbtn10");
+    tooltip.innerHTML = "Copied!";
+}
